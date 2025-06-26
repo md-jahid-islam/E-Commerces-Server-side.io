eb73fa13a0e4d3ce99cdfe986af0f306188a8611
md-jahid-islam <jahidulislamwebbd@gmail.com>
2025-06-26 20:18:03 +0600

    added to category page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,24 +8,25 @@ const createCategory = async (req, res)=>{
     if(!req?.file?.path) return res.status(400).send({error: "Category image is required!"});
 
       
-    // Upload Category Image
+    // ========== Upload Category Image =========== //
     const result = await cloudinary.uploader.upload(req.file.path, { folder: "categories"})
     
     fs.unlinkSync(req.file.path)
 
     const category = new categorySchema({
-        name,
-        image: result.url
+    name,
+    image: result.url
     })
 
     category.save()
 
     res.status(201).send({success: "Category created", category});
-}
+ }
 
-const getCategories = async (req, res)=>{
+ const getCategories = async (req, res)=>{
    const categories = await categorySchema.find()
    res.status(200).send(categories)
-}
+ }
 
-module.exports = {createCategory, getCategories}
+ module.exports = {createCategory, getCategories}
+ //  hahadjfkhakjdsfhasjkdfhajsdfha
